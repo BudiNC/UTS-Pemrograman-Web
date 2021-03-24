0ba1b73d490266468300f9b370f5331da48be2fa
Budi Nurcahyono <79905370+BudiNC@users.noreply.github.com>
2021-03-24 08:31:38 +0700

    Add files via upload

---
+++ TablecovidwilayahBanten.php
@@ -0,0 +1,48 @@
+<?php
+echo "<marquee><h1>Data Pemantaun Covid19 wilayah Banten M.Budi Nurcahyono/181011401603</h1></marquee>"; 
+	echo "Per ";
+	echo date('d-F-Y');
+	echo date(' h:i:s A');
+echo "<center>";
+echo "<table bgcolor =Blue=400 border =2>";
+
+echo "</tr>";
+echo "<td>Nama Wilayah</td>";
+echo "<td>Jumlah Positif</td>";
+echo "<td>Jumlah Dirawat</td>";
+echo "<td>Jumlah Sembuh</td>";
+echo "<td>Jumlah Meninggal</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>Banten</td>";
+echo "<td>41.230</td>";
+echo "<td>2.897</td>";
+echo "<td>37.227</td>";
+echo "<td>1.106</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>Jawa Barat</td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>DKI Jakarta</td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>Jawa Tengah</td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "</tr>";
+echo "<tr>";
+
+echo "</table>";
+echo "</center>";
+?>
